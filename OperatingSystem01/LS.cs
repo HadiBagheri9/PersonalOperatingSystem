7a32dcc0e9038649a26251e8395580df9bf94988
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-07-20 14:40:38 +0330

    Clean code LS

---
+++ OperatingSystem01/LS.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+
+namespace PersonalOperatingSystem
+{
+    class LS
+    {
+        public static void Ls(ref string cmd)
+        {
+            if (Vars.cmd.StartsWith("ls"))
+            {
+                Vars.showFilesAndFolders = Vars.cmd.Split(' ');
+
+                if (Vars.showFilesAndFolders.Length == 2)
+                {
+                    for (int l = 0; l < Vars.showFilesAndFolders.Length; l++)
+                    {
+                        if (l == 0)
+                        {
+                            Vars.cmd = Vars.showFilesAndFolders[l];
+                        }
+
+                        else if (l == 1)
+                        {
+
+                            Vars.ls = Vars.showFilesAndFolders[l];
+
+                            if (Vars.ls.CompareTo("-h") == 0/* || ls.CompareTo("-H") == 0*/)
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkGray;
+                                Console.WriteLine("ls <secend CMD> : Custom working");
+                                Console.WriteLine("<-h> : Show this message.");
+                                Console.WriteLine("<-d> : Show name of directoties.");
+                                Console.WriteLine("<-f> : Show name of files.>");
+                                Console.WriteLine("<-dc> : Show name of directories + Show Creation time.");
+                                Console.WriteLine("<-fc> : Show name of files + Show Creation time.");
+                            }
+
+                            else if (Vars.ls.CompareTo("-d") == 0 /*|| ls.CompareTo("-D") == 0*/)
+                            {
+                                Vars.directories = Directory.GetDirectories(Vars.path);
+
+                                foreach (string directory in Vars.directories)
+                                {
+                                    DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+
+                                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                                    Console.WriteLine(directoryInfo.Name);
+                                }
+                            }
+
+                            else if (Vars.ls.CompareTo("-f") == 0)
+                            {
+                                Vars.files = Directory.GetFiles(Vars.path);
+
+                                foreach (string file in Vars.files)
+                                {
+                                    FileInfo fileInfo = new FileInfo(file);
+
+                                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                                    Console.WriteLine(fileInfo.Name);
+                                }
+                            }
+
+                            else if (Vars.ls.CompareTo("-dc") == 0 /*|| ls.CompareTo("-D") == 0*/)
+                            {
+                                Vars.directories = Directory.GetDirectories(Vars.path);
+
+                                foreach (string directory in Vars.directories)
+                                {
+                                    DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+
+                                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                                    Console.WriteLine(directoryInfo.Name + "\t\t\t" + directoryInfo.CreationTime);
+                                }
+                            }
+
+                            else if (Vars.ls.CompareTo("-fc") == 0)
+                            {
+                                Vars.files = Directory.GetFiles(Vars.path);
+
+                                foreach (string file in Vars.files)
+                                {
+                                    FileInfo fileInfo = new FileInfo(file);
+
+                                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                                    Console.WriteLine(fileInfo.Name + "\t\t\t" + fileInfo.CreationTime);
+                                }
+                            }
+
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkGray;
+                                Console.WriteLine("Type \'ls -h\' to see help message.");
+                            }
+                        }
+                    }
+                }
+
+                else if (Vars.showFilesAndFolders.Length == 1)
+                {
+                    try
+                    {
+
+
+                        Vars.directories = Directory.GetDirectories(Vars.path);
+                        Vars.files = Directory.GetFiles(Vars.path);
+
+                        foreach (string directory in Vars.directories)
+                        {
+                            DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+
+                            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                            Console.WriteLine(directoryInfo.Name);
+                        }
+
+                        foreach (string file in Vars.files)
+                        {
+                            FileInfo fileInfo = new FileInfo(file);
+
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
+                            Console.WriteLine(fileInfo.Name);
+                        }
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Can't use this command in this path.");
+                    }
+                }
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Input is wrong.");
+                }
+            }
+        }
+    }
+}
--- OperatingSystem01/Program.cs
+++ OperatingSystem01/Program.cs
@@ -261,128 +261,7 @@ namespace PersonalOperatingSystem
                     }
                 }
                 //***ls***
-                else if (Vars.cmd.StartsWith("ls"))
-                {
-                    Vars.showFilesAndFolders = Vars.cmd.Split(' ');
-
-                    if (Vars.showFilesAndFolders.Length == 2)
-                    {
-                        for (int l = 0; l < Vars.showFilesAndFolders.Length; l++)
-                        {
-                            if (l == 0)
-                            {
-                                Vars.cmd = Vars.showFilesAndFolders[l];
-                            }
-
-                            else if (l == 1) {
-
-                                Vars.ls = Vars.showFilesAndFolders[l];
-
-                                if (Vars.ls.CompareTo("-h") == 0/* || ls.CompareTo("-H") == 0*/)
-                                {
-                                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                                    Console.WriteLine("ls <secend CMD> : Custom working");
-                                    Console.WriteLine("<-h> : Show this message.");
-                                    Console.WriteLine("<-d> : Show name of directoties.");
-                                    Console.WriteLine("<-f> : Show name of files.>");
-                                    Console.WriteLine("<-dc> : Show name of directories + Show Creation time.");
-                                    Console.WriteLine("<-fc> : Show name of files + Show Creation time.");
-                                }
-
-                                else if (Vars.ls.CompareTo("-d") == 0 /*|| ls.CompareTo("-D") == 0*/)
-                                {
-                                    Vars.directories = Directory.GetDirectories(Vars.path);
-
-                                    foreach (string directory in Vars.directories)
-                                    {
-                                        DirectoryInfo directoryInfo = new DirectoryInfo(directory);
-
-                                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                                        Console.WriteLine(directoryInfo.Name);
-                                    }
-                                }
-
-                                else if (Vars.ls.CompareTo("-f") == 0)
-                                {
-                                    Vars.files = Directory.GetFiles(Vars.path);
-
-                                    foreach (string file in Vars.files)
-                                    {
-                                        FileInfo fileInfo = new FileInfo(file);
-
-                                        Console.ForegroundColor = ConsoleColor.DarkCyan;
-                                        Console.WriteLine(fileInfo.Name);
-                                    }
-                                }
-
-                                else if (Vars.ls.CompareTo("-dc") == 0 /*|| ls.CompareTo("-D") == 0*/)
-                                {
-                                    Vars.directories = Directory.GetDirectories(Vars.path);
-
-                                    foreach (string directory in Vars.directories)
-                                    {
-                                        DirectoryInfo directoryInfo = new DirectoryInfo(directory);
-
-                                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                                        Console.WriteLine(directoryInfo.Name + "\t\t\t" + directoryInfo.CreationTime);
-                                    }
-                                }
-
-                                else if (Vars.ls.CompareTo("-fc") == 0)
-                                {
-                                    Vars.files = Directory.GetFiles(Vars.path);
-
-                                    foreach (string file in Vars.files)
-                                    {
-                                        FileInfo fileInfo = new FileInfo(file);
-
-                                        Console.ForegroundColor = ConsoleColor.DarkCyan;
-                                        Console.WriteLine(fileInfo.Name + "\t\t\t" + fileInfo.CreationTime);
-                                    }
-                                }
-
-                                else
-                                {
-                                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                                    Console.WriteLine("Type \'ls -h\' to see help message.");
-                                }
-                            }
-                        }
-                    }
-
-                    else if (Vars.showFilesAndFolders.Length == 1)
-                    {
-                        try {
-
-
-                            Vars.directories = Directory.GetDirectories(Vars.path);
-                            Vars.files = Directory.GetFiles(Vars.path);
-
-                            foreach (string directory in Vars.directories)
-                            {
-                                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                                Console.WriteLine(directory);
-                            }
-
-                            foreach (string file in Vars.files)
-                            {
-                                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                                Console.WriteLine(file);
-                            }
-                        }
-                        catch
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Can't use this command in this path.");
-                        }
-                    }
-
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Input is wrong.");
-                    }
-                }
+                LS.Ls(ref Vars.cmd);
 
                 Console.ResetColor();
 
@@ -470,13 +349,13 @@ namespace PersonalOperatingSystem
                     case "help":
 
                         Console.ForegroundColor = ConsoleColor.DarkGray;
-                        Console.WriteLine("\'cd <path>\' : To change path(use without \'<\' and \'>\').");
+                        Console.WriteLine("\'cd <path>\' : To change path(use without \'<\' and \'>\').");//
                         Console.WriteLine("\'read <filename>\' : To read text files(use without \'<\' and \'>\').");
                         Console.WriteLine("\'mkdir <foldername>\' : To make a directory(use without \'<\' and \'>\').");
                         Console.WriteLine("\'delfile <foldername>\' : To delete a file(use without \'<\' and \'>\').");
                         Console.WriteLine("\'deldir <foldername>\' : To delete a directory(use without \'<\' and \'>\').");
                         Console.WriteLine("\'touch <filename>\' : To make a file(use without \'<\' and \'>\').");
-                        Console.WriteLine("\'ls\' : To show files and directories.");
+                        Console.WriteLine("\'ls\' : To show files and directories.");//
                         Console.WriteLine("\'date\' : To show date of your system.");
                         Console.WriteLine("\'time\' : To show time of your system.");
                         Console.WriteLine("\'ip\' : To show IP list of your system.");
