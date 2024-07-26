f3a67dd9733bffb5182c9b1a9594477020e4aaa8
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-07-26 14:55:32 +0330

    Clean code TOUCH FUNC

--- OperatingSystem01/Program.cs
+++ OperatingSystem01/Program.cs
@@ -75,56 +75,8 @@ namespace PersonalOperatingSystem
                 MAKE_DIRECTORY.Make_Directory(ref Vars.cmd);
 
                 //***touch***
-                if (Vars.cmd.StartsWith("touch"))
-                {
-                    Vars.touch = Vars.cmd.Split(' ');
-                    if (Vars.touch.Length == 2)
-                    {
-                        for (int n = 0; n < Vars.touch.Length; n++)
-                        {
-                            if (n == 0)
-                            {
-                                Vars.cmd = Vars.touch[n];
-                            }
+                TOUCH.Touch(ref Vars.cmd);
 
-                            else if (n == 1)
-                            {
-                                Vars.fileMake = Vars.path + "\\" + Vars.touch[n];
-                                if (File.Exists(Vars.fileMake) == false)
-                                {
-                                    try
-                                    {
-                                        File.WriteAllText(Vars.fileMake, "\0");
-                                    }
-                                    catch
-                                    {
-                                        Console.ForegroundColor = ConsoleColor.Red;
-                                        Console.WriteLine("Faild to creat file.");
-                                    }
-                                }
-                                else
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.WriteLine("This name of file has used.");
-
-                                }
-                            }
-                        }
-                    }
-
-                    else if (Vars.touch.Length == 1)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGray;
-                        Console.WriteLine("touch <filename>");
-
-                    }
-
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Input is wrong.");
-                    }
-                }
                 //***delfile***
                 else if (Vars.cmd.StartsWith("delfile"))
                 {
---
+++ OperatingSystem01/TOUCH.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace PersonalOperatingSystem
+{
+    class TOUCH
+    {
+        public static void Touch(ref string cmd)
+        {
+            if (Vars.cmd.StartsWith("touch"))
+            {
+                Vars.touch = Vars.cmd.Split(' ');
+                if (Vars.touch.Length == 2)
+                {
+                    for (int n = 0; n < Vars.touch.Length; n++)
+                    {
+                        if (n == 0)
+                        {
+                            Vars.cmd = Vars.touch[n];
+                        }
+
+                        else if (n == 1)
+                        {
+                            Vars.fileMake = Vars.path + "\\" + Vars.touch[n];
+                            if (File.Exists(Vars.fileMake) == false)
+                            {
+                                try
+                                {
+                                    File.WriteAllText(Vars.fileMake, "\0");
+                                }
+                                catch
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Faild to creat file.");
+                                }
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("This name of file has used.");
+
+                            }
+                        }
+                    }
+                }
+
+                else if (Vars.touch.Length == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine("touch <filename>");
+
+                }
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Input is wrong.");
+                }
+            }
+        }
+    }
+}
