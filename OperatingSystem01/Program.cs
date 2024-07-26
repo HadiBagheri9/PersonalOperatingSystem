dcdd79d3bb5af836835d038ed20937a78ebe830e
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-07-26 14:57:31 +0330

    Clean code DEL_FILE FUNC

--- OperatingSystem01/Program.cs
+++ OperatingSystem01/Program.cs
@@ -78,53 +78,8 @@ namespace PersonalOperatingSystem
                 TOUCH.Touch(ref Vars.cmd);
 
                 //***delfile***
-                else if (Vars.cmd.StartsWith("delfile"))
-                {
-                    Vars.delFile = Vars.cmd.Split(' ');
+                DEL_FILE.Del_File(ref Vars.cmd);
 
-                    if (Vars.delFile.Length == 2)
-                    {
-                        for (int q = 0; q < Vars.delFile.Length; q++)
-                        {
-                            if (q == 0)
-                            {
-                                Vars.cmd = Vars.delFile[q];
-                            }
-                            else if (q == 1)
-                            {
-                                Vars.delFilePath = Vars.path + @"\" + Vars.delFile[q];
-
-                                if (File.Exists(Vars.delFilePath))
-                                {
-                                    try
-                                    {
-                                        File.Delete(Vars.delFilePath);
-                                    }
-                                    catch
-                                    {
-                                        Console.ForegroundColor = ConsoleColor.Red;
-                                        Console.WriteLine("Failed to delete file.");
-                                    }
-                                }
-                                else
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.WriteLine("File not found.");
-                                }
-                            }
-                        }
-                    }
-                    else if (Vars.delFile.Length == 1)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGray;
-                        Console.WriteLine("delfile <filename>");
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Input is wrong.");
-                    }
-                }
                 //***ls***
                 LS.Ls(ref Vars.cmd);
 
