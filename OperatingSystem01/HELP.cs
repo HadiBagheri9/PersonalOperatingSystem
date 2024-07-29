6474eb0f52fc9b2e21f46495fe551fa488ccdad4
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-07-29 23:01:46 +0330

    HELP FUNC

---
+++ OperatingSystem01/HELP.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalOperatingSystem
+{
+    class HELP
+    {
+        public static void Help(ref string cmd)
+        {
+            if (cmd.CompareTo("help") == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("\'cd <path>\' : To change path(use without \'<\' and \'>\').");//
+                Console.WriteLine("\'read <filename>\' : To read text files(use without \'<\' and \'>\').");
+                Console.WriteLine("\'mkdir <foldername>\' : To make a directory(use without \'<\' and \'>\').");
+                Console.WriteLine("\'delfile <foldername>\' : To delete a file(use without \'<\' and \'>\').");
+                Console.WriteLine("\'deldir <foldername>\' : To delete a directory(use without \'<\' and \'>\').");
+                Console.WriteLine("\'touch <filename>\' : To make a file(use without \'<\' and \'>\').");
+                Console.WriteLine("\'ls\' : To show files and directories.");
+                Console.WriteLine("\'date\' : To show date of your system.");
+                Console.WriteLine("\'time\' : To show time of your system.");
+                Console.WriteLine("\'ip\' : To show IP list of your system.");
+                Console.WriteLine("\'cls\' or \'clear\' : To clear and refresh window.");
+                Console.WriteLine("\'exit\' : To close this window.");
+                Console.WriteLine("\'shutdown\' : To shutdown your computer.");
+                Console.WriteLine("\'restart\' : To restart your computer.");
+                Console.WriteLine("\'help\' : To see this message.");
+                Console.WriteLine("\'\' : To");
+            }
+        }
+    }
+}
--- OperatingSystem01/Program.cs
+++ OperatingSystem01/Program.cs
@@ -162,25 +162,6 @@ namespace PersonalOperatingSystem
                         break;
 
                     case "help":
-
-                        Console.ForegroundColor = ConsoleColor.DarkGray;
-                        Console.WriteLine("\'cd <path>\' : To change path(use without \'<\' and \'>\').");//
-                        Console.WriteLine("\'read <filename>\' : To read text files(use without \'<\' and \'>\').");
-                        Console.WriteLine("\'mkdir <foldername>\' : To make a directory(use without \'<\' and \'>\').");
-                        Console.WriteLine("\'delfile <foldername>\' : To delete a file(use without \'<\' and \'>\').");
-                        Console.WriteLine("\'deldir <foldername>\' : To delete a directory(use without \'<\' and \'>\').");
-                        Console.WriteLine("\'touch <filename>\' : To make a file(use without \'<\' and \'>\').");
-                        Console.WriteLine("\'ls\' : To show files and directories.");//
-                        Console.WriteLine("\'date\' : To show date of your system.");
-                        Console.WriteLine("\'time\' : To show time of your system.");
-                        Console.WriteLine("\'ip\' : To show IP list of your system.");
-                        Console.WriteLine("\'cls\' or \'clear\' : To clear and refresh window.");
-                        Console.WriteLine("\'exit\' : To close this window.");
-                        Console.WriteLine("\'shutdown\' : To shutdown your computer.");
-                        Console.WriteLine("\'restart\' : To restart your computer.");
-                        Console.WriteLine("\'help\' : To see this message.");
-                        Console.WriteLine("\'\' : To");
-
                         break;
 
                     default:
